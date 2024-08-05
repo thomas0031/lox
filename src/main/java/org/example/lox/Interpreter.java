2db612bcdd1e4b352bb9e38f9333d2373abe2c7f
thomas0031 <46800269+thomas0031@users.noreply.github.com>
2024-08-05 23:00:53 +0300

    Scoping

--- src/main/java/org/example/lox/Interpreter.java
+++ src/main/java/org/example/lox/Interpreter.java
@@ -21,6 +21,26 @@ public class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
         stmt.accept(this);
     }
 
+    @Override
+    public Void visitBlockStmt(Stmt.Block stmt) {
+        executeBlock(stmt.statements, new Environment(environment));
+        return null;
+    }
+
+    void executeBlock(List<Stmt> statements,
+                      Environment environment) {
+        Environment previous = this.environment;
+        try {
+            this.environment = environment;
+
+            for (Stmt statement : statements) {
+                execute(statement);
+            }
+        } finally {
+            this.environment = previous;
+        }
+    }
+
     private String stringify(Object object) {
         if (object == null) return "nil";
 
--- src/main/java/org/example/lox/Parser.java
+++ src/main/java/org/example/lox/Parser.java
@@ -49,6 +49,7 @@ public class Parser {
 
     private Stmt statement() {
         if (match(PRINT)) return printStatement();
+        if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
         return expressionStatement();
     }
@@ -64,6 +65,18 @@ public class Parser {
         consume(SEMICOLON, "Expect ';' after expression.");
         return new Stmt.Expression(expr);
     }
+
+    private List<Stmt> block() {
+        List<Stmt> statements = new ArrayList<>();
+
+        while (!check(RIGHT_BRACE) && !isAtEnd()) {
+            statements.add(declaration());
+        }
+
+        consume(RIGHT_BRACE, "Expect '}' after block.");
+        return statements;
+    }
+
 
     // expression     → equality ;
     // equality       → comparison ( ( "!=" | "==" ) comparison )* ;
